189e7b5b3dcb2d9b8316908d7d5915a58691b051
Sumit Saini <sumitsaini5877@gmail.com>
2025-07-31 23:25:02 +0530

    small chnages

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://gst-matcher.vercel.app/"],  # React dev server
+    allow_origins=["https://gst-matcher.vercel.app"],  # React dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
